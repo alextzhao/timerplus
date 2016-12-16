6a7c48ac236e81475976ca27c4d8c1dcc9ebc177
Alex Zhao <alexzhao@seas.upenn.edu>
2016-12-16 06:27:12 -0500

    retrives data from database for rooms

--- db/mongo.js
+++ db/mongo.js
@@ -1,6 +1,6 @@
 var mongoose = require('mongoose');
 // Connection url (we use mongo lab as a cloud mongo db solution)
-var uri = "mongodb://admin:admin@ds133358.mlab.com:33358/timerplus"
+var uri = "mongodb://admin:admin@ds133358.mlab.com:33358/timerplus";
 
 mongoose.connect(uri, function (err) {
 	if (err) {
@@ -25,5 +25,5 @@ module.exports = {
 	Rooms: Rooms,
 	mongoose: mongoose,
 	//TODO: WHAT DO WE DO ABOUT THE COMMENTS????
-	db: db.collection('Rooms')
-}
+	roomsCollection: db.collection('Rooms')
+};
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('../db/mongo');
 
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'TimerPlus' });
+	mongo.Rooms.find(function(err, rooms) {
+		console.log(rooms);
+		res.render('index', { title: 'TimerPlus', rooms: rooms });
+
+	});
+
+	//res.send("homepage, under construction");
 });
 
 module.exports = router;
