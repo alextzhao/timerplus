ea91e523c08e6f19c445308428ba42135555d53a
Alex Zhao <alexzhao@seas.upenn.edu>
2016-12-16 03:01:13 -0500

    adds rooms database integration

--- db/mongo.js
+++ db/mongo.js
@@ -1,9 +1,29 @@
-var mongo = require('mongodb').MongoClient;
-// TODO: Fill in the URL of the mongo database.
-var uri = "mongodb://divinquantx:091496Alex@ds133358.mlab.com:33358/timerplus"
+var mongoose = require('mongoose');
+// Connection url (we use mongo lab as a cloud mongo db solution)
+var uri = "mongodb://admin:admin@ds133358.mlab.com:33358/timerplus"
 
-mongo.connect(uri, function (err, db) {
-	
-
-
+mongoose.connect(uri, function (err) {
+	if (err) {
+		throw err;
+	} else {
+		console.log("DB successfully connected");
+	}
 });
+
+var db = mongoose.connection;
+
+// schema for a particular room
+var roomSchema = new mongoose.Schema({
+	roomName: String,
+	password: String,
+	date: String
+});
+
+var Rooms = mongoose.model('Rooms', roomSchema);
+
+module.exports = {
+	Rooms: Rooms,
+	mongoose: mongoose,
+	//TODO: WHAT DO WE DO ABOUT THE COMMENTS????
+	db: db.collection('Rooms')
+}
